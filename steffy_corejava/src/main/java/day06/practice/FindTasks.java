57598b75eed83ce8a89d6cca3e34fa024bd5131f
steffydominic <steffybeena7@gmail.com>
2023-07-14 16:25:11 +0530

    day07.solved

--- steffy_corejava/src/main/java/day06/practice/FindTasks.java
+++ steffy_corejava/src/main/java/day06/practice/FindTasks.java
@@ -4,34 +4,32 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class FindTasks {
-	
+
 	static boolean findTaskByName(String name, List<Task> tasks) {
-		
-		for(Task value: tasks ) {
-			if(value.taskName.equals(name)) {
-				
+
+		for (Task value : tasks) {
+			if (value.taskName.equals(name)) {
+
 				System.out.println("found the name");
-				
+
 				return true;
 			}
 		}
 		System.out.println("found not match");
-		
+
 		return false;
 	}
-	
+
 	public static void main(String[] args) {
-		
+
 		List<Task> tasks = new ArrayList<>();
 		// Adding tasks to ArrayList
-//			tasks.add("hkuhi");
 		tasks.add(new Task("task1", 1));
 		tasks.add(new Task("task2", 2));
 		tasks.add(new Task("task3", 3));
-		
-		//calling class and its method
-		FindTasks.findTaskByName("task1",tasks);  //invoke the value
-	
-		
-}
+
+		// calling class and its method
+		FindTasks.findTaskByName("task1", tasks); // invoke the value
+
+	}
 }
