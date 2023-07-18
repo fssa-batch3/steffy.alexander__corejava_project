ad51bb4556106682c0fc8347acf213f81234ceff
steffydominic <steffybeena7@gmail.com>
2023-07-18 15:42:55 +0530

    day09.solved

---
+++ steffy_corejava/src/main/java/misc/day09/CollectionSortingDemoIgnoreCase.java
@@ -0,0 +1,20 @@
+package misc.day09;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class CollectionSortingDemoIgnoreCase {
+	public static void main(String[] args) {
+		ArrayList<String> cityNames = new ArrayList<String>();
+		cityNames.add("Delhi");
+		cityNames.add("Chennai");
+		cityNames.add("bangalore");
+
+		System.out.println("Before Sort:" + cityNames);
+		Collections.sort(cityNames);
+		
+		Collections.sort(cityNames, String::compareToIgnoreCase);
+		System.out.println("Converting to string: "+(String.join(",",cityNames)));
+		System.out.println("After Sort:" + cityNames);
+	}
+}
---
+++ steffy_corejava/src/main/java/misc/day09/ComparableInterfaceDemo.java
@@ -0,0 +1,5 @@
+package misc.day09;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Collections;
